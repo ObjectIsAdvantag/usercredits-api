8ed019f5722ff01a02a27d658695752341b4b423
ObjectIsAdvantag <steve.sfartz@gmail.com>
2016-06-10 13:43:02 +0200

    added Credits API via sails generate api credit

---
+++ api/controllers/CreditController.js
@@ -0,0 +1,11 @@
+/**
+ * CreditController
+ *
+ * @description :: Server-side logic for managing credits
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
